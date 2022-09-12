c397c3008ab91fc825e4d14984ab9db5b0e5ce7a
Alex <90658763+DESTHUbb@users.noreply.github.com>
2022-09-12 13:55:33 +0200

    Update test_connection_options.py

--- tests/test_connection_options.py
+++ tests/test_connection_options.py
@@ -13,6 +13,7 @@ from tests.test_databases import DATABASE_URLS, async_adapter
 from databases.backends.aiopg import AiopgBackend 
 from databases.backends.postgres import PostgresBackend
 from databases.core import DatabaseURL
+from tests.test_databases import DATABASE_URLS, async_adapter
 
 
 if sys.version_info >= (3, 7):  # pragma: no cover
