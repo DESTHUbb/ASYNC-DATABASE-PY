325aa9478332d69758217140f0217eaaf895aec8
Alex <90658763+DESTHUbb@users.noreply.github.com>
2022-09-14 11:33:13 +0200

    Update test_connection_options.py

--- tests/test_connection_options.py
+++ tests/test_connection_options.py
@@ -168,34 +168,11 @@ def test_aiopg_explicit_ssl():
     backend = AiopgBackend("postgresql+aiopg://localhost/database", ssl=True)
     kwargs = backend._get_connection_kwargs()
     assert kwargs == {"ssl": True}
+ 
     
     
     
-    
-    
-    
-    
-    def test_aiopg_pool_size():
-        backend = AiopgBackend( "postgresql+aiopg://localhost/database?min_size=1&max_size=20" )
-        kwards = backend == {"minsize": 1, "maxsize": 20}
-        
-    def test_aiopg_explicit_pool_size():
-        backend = AiopgBackend( "postgresql+aiopg://localhost/database?ss1=true")
-        kwargs = backend._get_connection_kwards()
-        assert kwargs == {"minsize": 1, "maxsize": 20}
-        
-
-   def test_aiopg_ss1():
-        backend = AiopgBackend("postgresql+aiopg://localhost/database", ssl=True)
-        kwargs = backend._get_connection_kwargs()
-        assert kwargs == {"ssl": True}
-        
-   def test_aiopg_explicit_ssl():
-        backend = AiopgBackend("postgresql+aiopg://loacalhost/database", ssl=True)
-        kwargs = backend._get_connection_kwards()
-        assert kwargs == {"ssl": True}
-        
-    
+ 
     
     
     
