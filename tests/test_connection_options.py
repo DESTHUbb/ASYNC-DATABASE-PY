d0ec7d89bf3203e22a415c79085effd8d451c3f5
Alex <90658763+DESTHUbb@users.noreply.github.com>
2022-09-15 12:22:30 +0200

    Update test_connection_options.py

--- tests/test_connection_options.py
+++ tests/test_connection_options.py
@@ -193,14 +193,6 @@ def test_aiopg_explicit_ssl():
     
     
     
-   
-
-@pytest.mark.skipif(sys.version_info >= (3, 10), reason="requires python3.9 or lower")
-def test_mysql_explicit_pool_size():
-    backend = MySQLBackend("mysql://localhost/database", min_size=1, max_size=20)
-    kwargs = backend._get_connection_kwargs()
-    assert kwargs == {"minsize": 1, "maxsize": 20}
-
 
 @pytest.mark.skipif(sys.version_info >= (3, 10), reason="requires python3.9 or lower")
 def test_mysql_ssl():
