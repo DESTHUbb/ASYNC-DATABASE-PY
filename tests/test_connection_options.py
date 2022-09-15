5d0f64421607bf28b3dc32153839d94c92754bd5
Alex <90658763+DESTHUbb@users.noreply.github.com>
2022-09-15 12:24:24 +0200

    Update test_connection_options.py

--- tests/test_connection_options.py
+++ tests/test_connection_options.py
@@ -199,11 +199,6 @@ def test_aiopg_explicit_ssl():
 
 
 
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
-def test_asyncmy_explicit_pool_size():
-    backend = AsyncMyBackend("mysql://localhost/database", min_size=1, max_size=20)
-    kwargs = backend._get_connection_kwargs()
-    assert kwargs == {"minsize": 1, "maxsize": 20}
 
 
 @pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
