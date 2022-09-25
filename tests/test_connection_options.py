9bcb04243c34f16a73b5443bc453ec521ba6e843
Alex <90658763+DESTHUbb@users.noreply.github.com>
2022-09-25 13:05:24 +0200

    Update test_connection_options.py

--- tests/test_connection_options.py
+++ tests/test_connection_options.py
@@ -181,6 +181,41 @@ def test_aiopg_explicit_ssl():
     
     
     
+
+    )
+    kwargs = backend._get_connection_kwargs()
+    assert kwargs == {"minsize": 1, "maxsize": 20}
+
+
+def test_aiopg_explicit_pool_size():
+    backend = AiopgBackend(
+        "postgresql+aiopg://localhost/database", min_size=1, max_size=20
+    )
+    kwargs = backend._get_connection_kwargs()
+    assert kwargs == {"minsize": 1, "maxsize": 20}
+
+
+def test_aiopg_ssl():
+    backend = AiopgBackend("postgresql+aiopg://localhost/database?ssl=true")
+    kwargs = backend._get_connection_kwargs()
+    assert kwargs == {"ssl": True}
+
+
+def test_aiopg_explicit_ssl():
+    backend = AiopgBackend("postgresql+aiopg://localhost/database", ssl=True)
+    kwargs = backend._get_connection_kwargs()
+    assert kwargs == {"ssl": True}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
